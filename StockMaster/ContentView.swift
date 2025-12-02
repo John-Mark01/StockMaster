86dc092d51fe9333286519f30fb1247f3231edba
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2025-12-02 07:45:38 +0200

    Initial Commit

---
+++ StockMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockMaster
+//
+//  Created by John-Mark Iliev on 2.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockMaster/StockMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMasterApp.swift
+//  StockMaster
+//
+//  Created by John-Mark Iliev on 2.12.25.
+//
+
+import SwiftUI
+
+@main
+struct StockMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
