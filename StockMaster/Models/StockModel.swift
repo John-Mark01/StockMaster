fd5d84a49dc526772d254a79ae67d6480985ff19
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2025-12-02 09:35:42 +0200

    created a StockModel for data transfer

---
+++ StockMaster/Models/StockModel.swift
@@ -0,0 +1,14 @@
+//
+//  StockModel.swift
+//  StockMaster
+//
+//  Created by John-Mark Iliev on 2.12.25.
+//
+
+import Foundation
+
+struct StockModel {
+    let name: String
+    let price: Double
+    let change: Double
+}
