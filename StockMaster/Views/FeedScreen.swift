a4b1436f1c2eb1d4990139c3cc5689d67920720f
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2025-12-03 21:20:44 +0200

    Added navigation from `FeedScreen` to `SymbolDetailsScreen`

--- StockMaster/Views/FeedScreen.swift
+++ StockMaster/Views/FeedScreen.swift
@@ -12,34 +12,45 @@ struct FeedScreen: View {
         webSocket: WebSocketServiceImpl()
     )
     
+    @State private var navigationPath = NavigationPath()
+    
     var body: some View {
-        List {
-            ForEach(viewModel.symbols.sorted(by: { $0.currentPrice > $1.currentPrice }), id: \.id) { symbol in
-                SymbolViewRow(symbol: symbol)
-            }
-        }
-        .animation(.easeIn(duration: 1.2), value: viewModel.elementWillChange)
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button(viewModel.connectionStatus == .connected ? "Stop" : "Start") {
-                    if viewModel.connectionStatus == .connected {
-                        viewModel.disconnectFromWebSocket()
-                    } else {
-                        viewModel.connectToWebSocket()
-                        viewModel.startUpdatingPrices()
-                    }
+        NavigationStack(path: $navigationPath) {
+            List {
+                ForEach(viewModel.symbols.sorted(by: { $0.currentPrice > $1.currentPrice }), id: \.id) { symbol in
+                    
+                    SymbolViewRow(symbol: symbol)
+                        .onTapGesture {
+                            navigationPath.append(symbol)
+                        }
                 }
-                .buttonStyle(.bordered)
-                .tint(viewModel.connectionStatus == .connected ? Color.red : Color.green)
             }
-            
-            ToolbarItem(placement: .topBarLeading) {
-                if viewModel.connectionStatus == .idle {
-                    ProgressView()
-                } else {
-                    Circle()
-                        .fill(viewModel.connectionStatus == .connected ? Color.green : Color.red)
-                        .frame(width: 30, height: 30)
+            .navigationDestination(for: SymbolModel.self) { symbol in
+                SymbolDetailsScreen(symbol: symbol)
+            }
+            .animation(.easeIn(duration: 1.2), value: viewModel.elementWillChange)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(viewModel.connectionStatus == .connected ? "Stop" : "Start") {
+                        if viewModel.connectionStatus == .connected {
+                            viewModel.disconnectFromWebSocket()
+                        } else {
+                            viewModel.connectToWebSocket()
+                            viewModel.startUpdatingPrices()
+                        }
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(viewModel.connectionStatus == .connected ? Color.red : Color.green)
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    if viewModel.connectionStatus == .idle {
+                        ProgressView()
+                    } else {
+                        Circle()
+                            .fill(viewModel.connectionStatus == .connected ? Color.green : Color.red)
+                            .frame(width: 30, height: 30)
+                    }
                 }
             }
         }
